d64bd88a61935b959d440b43ab7647d61f7660ab
JMJ010101 <130643670+JMJ010101@users.noreply.github.com>
2023-05-26 10:22:52 +0900

    Add files via upload

---
+++ src/components/ReviewStory/ReviewStory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Inner, ReviewContainer } from "./ReviewSty";
+
+const ReviewStory = () => {
+  return (
+    <ReviewContainer>
+      <Inner></Inner>
+    </ReviewContainer>
+  );
+};
+
+export default ReviewStory;
